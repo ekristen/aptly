eb583777012c5448b1e296a1c8353dd940cdfc67
Erik Kristensen <erik@erikkristensen.com>
2019-03-06 12:47:47 -0700

    Update gnupg.go

--- pgp/gnupg.go
+++ pgp/gnupg.go
@@ -89,7 +89,7 @@ func (g *GpgSigner) gpgArgs() []string {
 // NewGpgSigner creates a new gpg signer
 func NewGpgSigner(finder GPGFinder) *GpgSigner {
 	gpg, version, err := finder.FindGPG()
-	fmt.Sprintf("%s -> %s", gpg, version)
+	fmt.Printf("%s -> %s", gpg, version)
 	if err != nil {
 		panic(err)
 	}
